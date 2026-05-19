8efbcf6d60d236234c3be579e7a4e38ff43b7547
kehinde odedeji <odedejikehinde08@gmail.com>
2026-05-19 10:54:57 +0100

    updated blog

--- src/Data.js
+++ src/Data.js
@@ -4,7 +4,7 @@ const Data = {
     username: "caleb@system",
     role: "Product-focused | Software Engineer",
     currentStatus: "Building Odeshi",
-    blogLink: "https://medium.com/me/stories",
+    blogLink: "https://medium.com/@odedejikehinde08",
     projects: "https://zesty-diascia-c1a.notion.site/Caleb-Odedeji-Software-Engineer-3234575361568077b7cac7c326e739f5",
     contact: {
         email: "kehindeodedeji04@gmail.com",
